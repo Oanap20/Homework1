380112826ae1fb8f7e993bf958fd8b586d021b3d
Oanap20 <68903950+Oanap20@users.noreply.github.com>
2020-08-06 13:14:26 +0300

    Update jQueryJS.js

--- jQueryJS.js
+++ jQueryJS.js
@@ -93,6 +93,130 @@ $(document).ready(function () {
         $("body").append("" + division + " elements found");
     }
 
-    // eighteen();
+    //eighteen();
 
+    function nineteen() {
+        $("#button-seven").click(function () {
+            $("#pid").animate({
+                width: "70%",
+                opacity: 0.4,
+                marginLeft: "0.6in",
+                fontSize: "3em",
+                borderWidth: "10px"
+            }, 3000, function () {
+
+            })
+        })
+    }
+
+    //nineteen();
+
+    function twenty() {
+        $("#right").click(function () {
+            $(".block-div").animate({
+                "left": "+=50px"
+            }, "linear");
+        });
+
+        $("#left").click(function () {
+            $(".block-div").animate({
+                "left": "-=50px"
+            }, "linear");
+        });
+    }
+
+    // twenty();
+
+
+    function twentyone() {
+        $("#run").click(function () {
+            $(".block:first").animate({
+                left: 400
+            }, {
+                duration: 3000,
+                step: function (now, fx) {
+                    $(".block:gt(0)").css("left", now);
+
+                }
+            });
+        });
+
+    }
+
+    //twentyone();
+
+    function twentytwo() {
+        $("#button-nine").click(function () {
+            $(".nine:animated").toggleClass("nine.colored");
+        });
+    }
+
+    function animateIt() {
+        $("#div1,#div2").slideToggle("slow", animateIt);
+    }
+
+    twentytwo();
+    animateIt();
+
+
+    function twentythree() {
+        $("body p").append("<p>append text to paragraphs</p>")
+    }
+
+    //twentythree();
+
+
+    function twentyfour() {
+        $("p").append($("strong"))
+    }
+
+    //twentyfour();
+
+
+    function twentyfive() {
+        $("p").append(document.createTextNode(" hello i am a text node byee"));
+    }
+
+    // twentyfive();
+
+
+    function twentysix() {
+        $("span").appendTo("#myid")
+    }
+
+    twentysix();
+
+    function twentyseven() {
+        $("input")
+            .change(function () {
+                var $input = $(this);
+                console.log(".attr( 'checked' )");
+                console.log(".prop( 'checked' ): " + $input.prop("checked"));
+                console.log(".is( ':checked' ): " + $input.is(":checked"));
+            })
+            .change();
+    }
+
+    twentyseven();
+
+
+    function twentyeight() {
+        var title = $("em").attr("title");
+        $("#id1").text(title)
+    }
+
+    twentyeight();
+
+
+    function twentynine() {
+        $("a[hreflang|='de']").css("border", "3px solid pink");
+    }
+
+    twentynine();
+
+    function thirty() {
+        $("div[name*='tutorial']").css("background", "orange");
+    }
+
+    thirty();
 });
